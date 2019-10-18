f5b2775726edb07914f70b7cc10144cfa1cb7b97
David Codina <davidmcodina@hotmail.com>
2019-10-18 16:09:01 -0600

    Updates

---
+++ precache-manifest.6e2b9f2d77f4bd8a6294302dda15de5f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "134ce84741584ba2c01a074cccb09b64",
+    "url": "/react-mock-portfolio-site/index.html"
+  },
+  {
+    "revision": "e167e5757e1373b2af62",
+    "url": "/react-mock-portfolio-site/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "c8eb114b667bb0d0cb5b",
+    "url": "/react-mock-portfolio-site/static/css/main.70c24eda.chunk.css"
+  },
+  {
+    "revision": "e167e5757e1373b2af62",
+    "url": "/react-mock-portfolio-site/static/js/2.4bb37d25.chunk.js"
+  },
+  {
+    "revision": "c8eb114b667bb0d0cb5b",
+    "url": "/react-mock-portfolio-site/static/js/main.91bf321b.chunk.js"
+  },
+  {
+    "revision": "e1c548b7665a8f68dc31",
+    "url": "/react-mock-portfolio-site/static/js/runtime-main.f3215f4c.js"
+  },
+  {
+    "revision": "4bd726fb31bef49dc19c1f6e06315d0d",
+    "url": "/react-mock-portfolio-site/static/media/avatar-2.4bd726fb.png"
+  },
+  {
+    "revision": "8201003de5cbd5e130eabae9d735c3b6",
+    "url": "/react-mock-portfolio-site/static/media/avatar-3.8201003d.png"
+  },
+  {
+    "revision": "da5405f895e605e7a98bbb642e76f87a",
+    "url": "/react-mock-portfolio-site/static/media/avatar-4.da5405f8.png"
+  },
+  {
+    "revision": "6d10ae4e662768762c9af49d5d1727e7",
+    "url": "/react-mock-portfolio-site/static/media/avatar.6d10ae4e.png"
+  },
+  {
+    "revision": "e102fba9d0b832a4962a19cc64959179",
+    "url": "/react-mock-portfolio-site/static/media/halftone.e102fba9.png"
+  }
+]);
